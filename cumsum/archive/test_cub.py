024291150ae0f97875c4369ed92cf867e5bf1666
Clement Chan <iclementine@outlook.com>
2025-08-17 08:17:15 +0800

    move to talk dir

--- cumsum/archive/test_cub.py
+++
@@ -1,3 +0,0 @@
-import torch
-a = torch.randn((4566778,), device="cuda")
-b = torch.cumsum(a, 0)
--- sort/archive/profile.py
+++
@@ -1,4 +0,0 @@
-import torch
-x = torch.randint(0, 2, (16 * 1024,), dtype=torch.uint16, device="cuda")
-out = torch.sort(x)
-print(out)
